f7d935433d5d0b377d069933463ee0b8b2b30193
kenmwangi <kenmwangi071@gmail.com>
2024-07-04 09:38:49 +0300

    SEO

---
+++ data/resume.tsx
@@ -0,0 +1,45 @@
+import { Icons } from "@/components/Icons";
+
+export const DATA = {
+  name: "Ken Mwangi",
+  initials: "KM",
+  url: "https://www.kenmwangi.com",
+  location: "Nairobi, Kenya",
+  locationLink: "https://www.google.com/maps/place/nairobi",
+  description:
+    "Experienced software engineer specializing in building extensive full-stack systems that are reliable, scalable, and feature a great user interface",
+  summary: "I am into building and scaling my own SaaS businesses.",
+  avatarUrl: "/me.jpg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Go",
+    "Postgres",
+    "Docker",
+    "Kubernetes",
+    "Java",
+    "C++",
+  ],
+
+  contact: {
+    email: "kenmwangi071@gmail.com",
+    tel: "+254717964123",
+    social: {
+      GitHub: {
+        url: "https://github.com/kenmwangi",
+        icon: Icons.github,
+      },
+      LinkedIn: {
+        url: "https://www.linkedin.com/in/kkmwangi/",
+        icon: Icons.linkedin,
+      },
+      X: {
+        url: "https://x.com/ken_cipher",
+        icon: Icons.x,
+      },
+    },
+  },
+};
